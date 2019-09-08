393f52ae83f93b861f0098b5490b12d00467be13
Santiago Mondragon <santiagomondram@gmail.com>
2019-09-08 17:21:54 -0500

    About Page Almost Done

--- js/main.js
+++ js/main.js
@@ -2,3 +2,30 @@ let menuItems = document.querySelectorAll("body #navbar.sideBar #menu li");
 let menuItemWidth = menuItems[0];
 
 let about = document.getElementById("about");
+
+
+let skills = [...document.querySelectorAll("#skills .options .icons i")]
+let skillDisplay = document.querySelector("#skills .options .display")
+
+skills.forEach((e, i) => {
+    e.addEventListener('click', ()=> {
+        for (let i = 0; i < skills.length; i++) {
+            if(skills[i].classList[2]){
+                skills[i].classList.remove("active");
+            }
+        }
+        e.classList.add("active");
+        if(i == 0){
+            skillDisplay.className = "display ui";
+        }
+        if(i == 1){
+            skillDisplay.className = "display ux";
+        }
+        if(i == 2){
+            skillDisplay.className = "display web";
+        }
+        if(i == 3){
+            skillDisplay.className = "display research";
+        }
+    })
+});
