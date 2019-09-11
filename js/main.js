aa925d8acf909daaee757a12f5b106b671ed8e83
Santiago Mondragon <santiagomondram@gmail.com>
2019-09-11 16:58:49 -0500

    Basic Structure is complete

--- js/main.js
+++ js/main.js
@@ -29,3 +29,29 @@ skills.forEach((e, i) => {
         }
     })
 });
+
+let expertise = [...document.querySelectorAll("#expertise .options .icons i")]
+let expertiseDisplay = document.querySelector("#expertise .options .display")
+
+expertise.forEach((e, i) => {
+    e.addEventListener('click', ()=> {
+        for (let i = 0; i < expertise.length; i++) {
+            if(expertise[i].classList[2]){
+                expertise[i].classList.remove("active");
+            }
+        }
+        e.classList.add("active");
+        if(i == 0){
+            expertiseDisplay.className = "display leader";
+        }
+        if(i == 1){
+            expertiseDisplay.className = "display team";
+        }
+        if(i == 2){
+            expertiseDisplay.className = "display analytic";
+        }
+        if(i == 3){
+            expertiseDisplay.className = "display business";
+        }
+    })
+});
